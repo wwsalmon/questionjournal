65a46a058fd32cb2d530a7979e0d1aea32c254c3
Samson Zhang <wwsamson@yahoo.com>
2021-06-07 18:19:04 -0700

    fix navbar link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import {useRouter} from "next/router";
 import QjButton from "./QjButton";
 import MoreMenu from "./MoreMenu";
 import MoreMenuItem from "./MoreMenuItem";
+import Link from "next/link";
 
 export default function Navbar() {
     const [session, loading] = useSession();
@@ -11,7 +12,11 @@ export default function Navbar() {
 
     return (
         <div className="h-20 flex items-center px-4 sm:px-10 fixed top-0 left-0 right-0">
-            <p className="uppercase font-black text-xl text-black">Question Journal</p>
+            <Link href={session ? "/app" : "/"}>
+                <a>
+                    <p className="uppercase font-black text-xl text-black">Question Journal</p>
+                </a>
+            </Link>
             <div className="ml-auto">
                 {(session && router.route !== "/") ? (
                     <MoreMenu customButton={(
@@ -26,7 +31,7 @@ export default function Navbar() {
                 ) : (
                     <div className="flex items-center">
                         <Button href="/auth/signin" className="mr-4">Sign in</Button>
-                        <QjButton href="#waitlist">Sign up for waitlist</QjButton>
+                        <QjButton href="/#waitlist">Sign up for waitlist</QjButton>
                     </div>
                 )}
             </div>
